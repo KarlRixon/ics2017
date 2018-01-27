ded941bec4cd25e08bbdd0a286046f838655fc55
tracer-ics2017 <tracer@njuics.org>
2018-01-27 21:38:17 +0800

    >  compile 161620222 karl Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  21:38:17 up  9:35,  1 user,  load average: 0.16, 0.04, 0.01 5141e33cd9a17a704060928d4d32e69ec87f72ef

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -14,8 +14,8 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  * For more details about the register encoding scheme, see i386 manual.
  */
 
-typedef struct {
-	union{
+typedef union {
+
 	   	union {
 			uint32_t _32;
 			uint16_t _16;
@@ -31,7 +31,7 @@ typedef struct {
 			rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
 			vaddr_t eip;
 		};
-	};
+
 } CPU_state;
 
 extern CPU_state cpu;
