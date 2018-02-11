57cc02736cbec3e806b4a0d9a42b73578b153d2f
tracer-ics2017 <tracer@njuics.org>
2018-02-11 20:23:21 +0800

    >  compile 161620222 karl Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  20:23:21 up  9:08,  7 users,  load average: 0.00, 0.01, 0.00 4249a0e9ea13d436969b739c1cb0314632ea6525

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -90,8 +90,8 @@ static bool make_token(char *e) {
   while (e[position] != '\0') {
     /* Try all rules one by one. */
     for (i = 0; i < NR_REGEX; i ++) {
-	  printf("tring %d : %s\n", i, rules[i].regex);
-      if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
+	  //printf("tring %d : %s\n", i, rules[i].regex);
+      if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0){
         char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
 
