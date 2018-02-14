e7bf8b35060e31c6032455041eff805a926f0b7f
tracer-ics2017 <tracer@njuics.org>
2018-02-14 13:41:25 +0800

    >  compile 161620222 karl Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  13:41:25 up  4:31,  6 users,  load average: 0.00, 0.00, 0.00 b806f192d4924326b40850bf889633c6d4bd83d8

--- nemu/include/monitor/watchpoint.h
+++ nemu/include/monitor/watchpoint.h
@@ -15,7 +15,7 @@ typedef struct watchpoint {
 } WP;
 
 void show_wp();
-WP *new_wp(char *str,int value);
+WP* new_wp(char *str, int value);
 void free_wp(int id);
 
 #endif
--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -1,6 +1,8 @@
 #include "monitor/watchpoint.h"
 #include "monitor/expr.h"
 
+#include <string.h>
+
 #define NR_WP 32
 
 static WP wp_pool[NR_WP];
@@ -28,4 +30,65 @@ void show_wp(){
 		printf(c_bold "wp[%d]		%s		%d\n" c_normal, p->NO,p->str,p->value);
 		p = p->next;
 	}
+}
+
+WP* new_wp(char *str, int value){
+	if(free_ == NULL){
+		printf(c_red c_bold "too many watchpoints\ntry to change NR_WP\n" c_normal);
+		assert(0);
+	}
+	WP *n = NULL;
+	n = free_;
+	free_ = free_->next;
+
+	n->value = value;
+	strcpy(n->str, str);
+	n->next = NULL;
+
+	if(head == NULL){
+		head = n;
+	}
+	else{
+		n->next = head;
+		head = n;
+	}
+	return n;
+}
+
+void free_wp(int no){
+	WP *p = head;
+	if(p == NULL){
+		printf(c_red c_bold "no watchpoints\n" c_normal);
+		return;
+	}
+	else if(p->NO == no){
+		memset(p->str, 0, sizeof(p->str)-1);
+		p->value = 0;
+		
+		head = p->next;
+		p->next = free_;
+		free_ = p;
+		return;
+	}
+	else{
+		WP *q = head;
+		p = q->next;
+		while(p != NULL){
+			if(p->NO == no){
+				memset(p->str, 0, sizeof(p->str)-1);
+				p->value = 0;
+
+				head = p->next;
+				p->next = free_;
+				free_ = p;
+				return;
+			}
+			else{
+				q = q->next;
+				p = p->next;
+			}
+		}
+	}
+	printf(c_red c_bold "no such watchpoint\n" c_normal);
+	return;
 }
