5dc91ebf2eb048fe2e7c715c4a44c995a9bdf289
tracer-ics2017 <tracer@njuics.org>
2018-03-13 22:23:35 +0800

    >  compile 161620222 karl Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  22:23:35 up  5:57, 10 users,  load average: 0.00, 0.00, 0.00 fece4f75259f9fee03107e19669899fed5fb97

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -174,9 +174,11 @@ static int cmd_x(char *args){
 	//	}
 	//	printf("\n");
 	//}
+	printf(c_bold "addr\t\t" c_yellow "Big-Endian\t\t" c_cyan "Little-Endian\n" c_normal);
 	for(i = 0;i < N; ++i){
 		uint32_t data = vaddr_read(Addr + i*4, 4);
-		printf(c_cyan c_bold "0x%08x: " c_normal,Addr + i*4);
+		printf(c_cyan c_bold "0x%08x: " c_normal, Addr + i*4);
+		printf(c_yellow "0x%08x" c_normal, data);
 		for(int j=0; j<4;++j){
 			printf(c_bold "0x%02x " c_normal, data & 0xff);
 			data = data >> 8;
