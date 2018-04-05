87ea59418923b87523e467c985c6adf0f46d74ad
tracer-ics2017 <tracer@njuics.org>
2018-04-05 15:59:15 +0800

    >  compile 161620222 karl Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  15:59:15 up  6:43, 12 users,  load average: 0.00, 0.01, 0.00 2c860acff013fac95262d56e83d39f12c979fa8a

--- nemu/src/cpu/exec/data-mov.c
+++ nemu/src/cpu/exec/data-mov.c
@@ -8,7 +8,7 @@ make_EHelper(mov) {
 make_EHelper(push) {
   // TODO();
 
-  printf("id_dest->val = %d | 0x%x\tid_dest->str = %s\n", id_dest->val, id_dest->val, id_dest->str);
+  // printf("id_dest->val = %d | 0x%x\tid_dest->str = %s\n", id_dest->val, id_dest->val, id_dest->str);
   rtl_push(&id_dest->val, id_dest->width);
 
   print_asm_template1(push);
