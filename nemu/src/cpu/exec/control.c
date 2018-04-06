d43b8f93218817c59a824f0a505cf716013cd426
tracer-ics2017 <tracer@njuics.org>
2018-04-06 17:36:08 +0800

    >  compile 161620222 karl Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  17:36:08 up 1 day,  8:19, 15 users,  load average: 0.00, 0.00, 0.00 c4bf6a392716793eac112ec3fe4dcb99523c4916

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -37,7 +37,7 @@ make_EHelper(call) {
 make_EHelper(ret) {
   // TODO();
 
-  
+  printf("src1 = 0x%08x\tsrc2 = 0x%08x\tdest = 0x%08x\n)", id_src->val, id_src2->val, id_dest->val);
 
   print_asm("ret");
 }
