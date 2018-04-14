5937b01920b6cb1feaf4ec7f450caeb839636f98
tracer-ics2017 <tracer@njuics.org>
2018-04-15 01:00:01 +0800

    >  compile 161620222 karl Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  01:00:01 up 1 day,  9:12, 15 users,  load average: 0.00, 0.00, 0.00 4f8d7d74e8834dff7441760e2f7ca7eea6697c8a

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -37,9 +37,10 @@ make_EHelper(sub) {
 make_EHelper(cmp) {
   // TODO();
 
-  
-  printf("src->width = %d\tsrc2->width = %d\tdest->width = %d\n", id_src->width, id_src2->width, id_dest->width);
+  printf("src->type = %d\tsrc2->type = %d\tdest->type = %d\n", id_src->type, id_src2->type, id_dest->type);
+  // printf("src->width = %d\tsrc2->width = %d\tdest->width = %d\n", id_src->width, id_src2->width, id_dest->width);
   // printf("src->val = 0x%08x\tsrc2->val = 0x%08x\tdest->val = 0x%08x\n", id_src->val, id_src2->val, id_dest->val);
+  
   print_asm_template2(cmp);
 }
 
