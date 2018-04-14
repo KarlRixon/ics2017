7a05598afaebdef86bde0b4a7d77531f6442347a
tracer-ics2017 <tracer@njuics.org>
2018-04-14 20:21:51 +0800

    >  compile 161620222 karl Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  20:21:51 up 1 day,  4:34, 15 users,  load average: 0.00, 0.00, 0.00 632c5e0fbc0adb65a6f57305c54fa3df6b1798d6

--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -14,14 +14,33 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   // TODO: Query EFLAGS to determine whether the condition code is satisfied.
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
+    case CC_O:
+		rtl_get_OF(dest);
+		printf("t0 = 0x%04x\tdest = 0x%04x\n", t0, *dest);
+		break;
+    case CC_B:
+		rtl_get_CF(dest);
+		printf("t0 = 0x%04x\tdest = 0x%04x\n", t0, *dest);
+		break;
+    case CC_E:
+		rtl_get_ZF(dest);
+		printf("t0 = 0x%04x\tdest = 0x%04x\n", t0, *dest);
+		break;
+    case CC_BE:
 		rtl_get_CF(&t0);
 		rtl_get_ZF(&t1);
 		rtl_or(&t0, &t0, &t1);
-		printf("t0 = 0x%04x\n", t0);
+		printf("t0 = 0x%04x\tdest = 0x%04x\n", t0, *dest);
+		rtl_andi(&t0, &t0, 0x1);
+		rtl_mv(dest, &t0);
+		break;
     case CC_S:
+		printf("t0 = 0x%04x\tdest = 0x%04x\n", t0, *dest);
     case CC_L:
+		printf("t0 = 0x%04x\tdest = 0x%04x\n", t0, *dest);
     case CC_LE:
-      TODO();
+		printf("t0 = 0x%04x\tdest = 0x%04x\n", t0, *dest);
+      // TODO();
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
