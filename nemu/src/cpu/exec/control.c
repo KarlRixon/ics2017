ffe24c745db0e0a692134d32ca78a00cfa1dbf9b
tracer-ics2017 <tracer@njuics.org>
2018-04-15 10:35:57 +0800

    >  compile 161620222 karl Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  10:35:57 up 1 day, 18:48, 15 users,  load average: 0.00, 0.00, 0.00 9922eb4022adc15f5f74036937662afb49aa8354

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -12,7 +12,7 @@ make_EHelper(jcc) {
   uint8_t subcode = decoding.opcode & 0xf;
   rtl_setcc(&t2, subcode);
   // printf("subcode = %x\tZF = %d\n", subcode, cpu.ZF);
-  decoding.jmp_eip += id_dest->val;
+  decoding.jmp_eip = decoding.seq_eip + id_dest->val;
   decoding.is_jmp = t2;
 
   print_asm("j%s %x", get_cc_name(subcode), decoding.jmp_eip);
