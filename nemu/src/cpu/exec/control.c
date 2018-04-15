1ef5a345d6160cdec3692b1cb987a16068e420d9
tracer-ics2017 <tracer@njuics.org>
2018-04-15 10:32:52 +0800

    >  compile 161620222 karl Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  10:32:52 up 1 day, 18:45, 15 users,  load average: 0.00, 0.00, 0.00 581c22e6fe1a07c369326c2356299cd1d98f7f1e

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -10,8 +10,8 @@ make_EHelper(jmp) {
 make_EHelper(jcc) {
   // the target address is calculated at the decode stage
   uint8_t subcode = decoding.opcode & 0xf;
-  printf("subcode = %x\tZF = %d\n", subcode, cpu.ZF);
   rtl_setcc(&t2, subcode);
+  printf("subcode = %x\tZF = %d\n", subcode, cpu.ZF);
   decoding.is_jmp = t2;
 
   print_asm("j%s %x", get_cc_name(subcode), decoding.jmp_eip);
