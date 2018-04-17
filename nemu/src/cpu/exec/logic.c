2d474ddbde571781be99576710f3cbb9796727eb
tracer-ics2017 <tracer@njuics.org>
2018-04-17 22:29:10 +0800

    >  compile 161620222 karl Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  22:29:10 up  5:27, 14 users,  load average: 0.00, 0.00, 0.00 ccae2f6b2e2c37c1d70eeab8b3e7389cb3b6f2b

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -4,7 +4,7 @@ make_EHelper(test) {
   // TODO();
 
   // printf("src1->width = %d\tdest->width = %ddest->type = %d\n", id_src->width, id_dest->width, id_dest->type);
-  printf("src1 = 0x%08x\tsrc2 = 0x%08x\tdest = 0x%08x\tdest->reg = 0x%08x\tdest->str = %s\n", id_src->val, id_src2->val, id_dest->val,id_dest->reg, id_dest->str);
+  printf("src1 = 0x%08xsrc1->reg = 0x%08x\tsrc2 = 0x%08x\tdest = 0x%08x\tdest->reg = 0x%08x\tdest->str = %s\n", id_src->val, id_src->reg, id_src2->val, id_dest->val,id_dest->reg, id_dest->str);
   rtl_and(&t0, &id_src->val, &id_dest->val);
   operand_write(id_dest, &t0);
   rtl_set_OF(&tzero);
