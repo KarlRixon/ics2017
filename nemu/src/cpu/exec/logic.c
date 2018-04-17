c3d9962a100fbcf40c5670363dac4867f8d40ef1
tracer-ics2017 <tracer@njuics.org>
2018-04-17 21:24:05 +0800

    >  compile 161620222 karl Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  21:24:05 up  4:22, 14 users,  load average: 0.01, 0.01, 0.00 7e79acb8e7acaeb632ff9665c524e8ea8e97c845

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -77,11 +77,11 @@ make_EHelper(sar) {
 make_EHelper(shl) {
   // TODO();
   // unnecessary to update CF and OF in NEMU
-  printf("src1->width = %d\tdest->width = %ddest->width = %d\n", id_src->width, id_dest->width, id_dest->width);
-  printf("src1 = 0x%08x\tsrc2 = 0x%08x\tdest->reg = 0x%08x\n", id_src->val, id_src2->val, id_dest->reg);
-  rtl_lr(&t0, id_src->reg, id_src->width);
-  printf("id_src->regval = 0x%08x\n", t0);
-  
+  // printf("src1->width = %d\tdest->width = %ddest->width = %d\n", id_src->width, id_dest->width, id_dest->width);
+  // printf("src1 = 0x%08x\tsrc2 = 0x%08x\tdest->reg = 0x%08x\n", id_src->val, id_src2->val, id_dest->reg);
+  // rtl_lr(&t0, id_src->reg, id_src->width);
+  // printf("id_src->regval = 0x%08x\n", t0);
+  rtl_shl(&id_dest->reg, &id_dest->reg, &id_src->reg);
 
   print_asm_template2(shl);
 }
