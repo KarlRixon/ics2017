93b7c49de9bef6ac6ad3137afb3c228d0387834e
tracer-ics2017 <tracer@njuics.org>
2018-04-17 21:59:32 +0800

    >  compile 161620222 karl Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  21:59:32 up  4:57, 14 users,  load average: 0.00, 0.00, 0.00 6fd1229174c4f94fbb01f3c1f8123e5d6899bfbf

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -96,7 +96,7 @@ make_EHelper(shr) {
 make_EHelper(setcc) {
 	uint8_t subcode = decoding.opcode & 0xf;
 	// printf("opcode = 0x%0x", decoding.opcode);
-	printf("eflags = 0x%08x\n", cpu.eflags);
+	// printf("eflags = 0x%08x\n", cpu.eflags);
 	// printf("src1->width = %d\tdest->width = %d\tsrc->type = %d\tdest->type = %d\n", id_src->width, id_dest->width, id_src->type, id_dest->type);
 	rtl_setcc(&t2, subcode);
 	
@@ -112,7 +112,7 @@ make_EHelper(setcc) {
 	else assert(0);
 	t0 += t2;
 	operand_write(id_dest, &t0);
-	// printf("t0 = 0x%08x\tt2 = 0x%80x\tsrc1 = 0x%08x\tsrc2 = 0x%08x\tdest = 0x%08x\n", t0, t2, id_src->val, id_src2->val, id_dest->val);
+	printf("t0 = 0x%08x\tt2 = 0x%80x\tsrc1 = 0x%08x\tsrc2 = 0x%08x\tdest = 0x%08x\n", t0, t2, id_src->val, id_src2->val, id_dest->val);
 	print_asm("set%s %s", get_cc_name(subcode), id_dest->str);
 }
 
