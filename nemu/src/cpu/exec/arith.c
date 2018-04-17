21de6bdc930c31c6fbb4b0ca87bedd9bf43c4ab6
tracer-ics2017 <tracer@njuics.org>
2018-04-17 23:56:11 +0800

    >  compile 161620222 karl Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  23:56:11 up  6:54, 14 users,  load average: 0.02, 0.01, 0.00 63d16f65241b5c2ade47ff53c56ff311cd8cb9bc

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -66,8 +66,17 @@ make_EHelper(inc) {
 }
 
 make_EHelper(dec) {
-  TODO();
-
+  // TODO();
+  rtl_sext(&id_dest->val, &id_dest->val, id_dest->width);
+  rtl_subi(&t0, &id_dest->val, 0x1);
+  operand_write(id_dest, &t0);
+  rtl_update_ZFSF(&t0, id_dest->width);
+  
+  rtl_xor(&t1, &id_dest->val, &id_src->val);
+  rtl_xor(&t2, &t0, &id_dest->val);
+  rtl_and(&t1, &t1, &t2);
+  rtl_msb(&t1, &t1, id_dest->width);
+  rtl_set_OF(&t1);
   print_asm_template1(dec);
 }
 
