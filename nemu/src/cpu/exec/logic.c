e0cb5f6abb37dc8d41b038ba04e4a753a3847043
tracer-ics2017 <tracer@njuics.org>
2018-04-17 21:39:02 +0800

    >  compile 161620222 karl Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  21:39:02 up  4:37, 14 users,  load average: 0.00, 0.00, 0.00 e880ad245c5d474b4e731eaa2ae227ace1b0f24

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -77,7 +77,7 @@ make_EHelper(sar) {
 make_EHelper(shl) {
   // TODO();
   // unnecessary to update CF and OF in NEMU
-  // printf("src1->width = %d\tdest->width = %ddest->width = %d\n", id_src->width, id_dest->width, id_dest->width);
+  printf("src1->str = %s\tsrc2->str = %sdest->str = %s\n", id_src->str, id_src2->str, id_dest->str);
   printf("src1->reg = 0x%08x\tsrc2 = 0x%08x\tdest->reg = 0x%08x\n", id_src->reg, id_src2->val, id_dest->reg);
   // rtl_lr(&t0, id_src->reg, id_src->width);
   rtl_shl(&t0, &id_dest->reg, &id_src->reg);
