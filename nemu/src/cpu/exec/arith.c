0a4838deaa3974bcee0bd0a351014ef986ad554d
tracer-ics2017 <tracer@njuics.org>
2018-04-18 11:45:30 +0800

    >  compile 161620222 karl Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  11:45:30 up  1:36, 18 users,  load average: 0.00, 0.01, 0.00 8b3c3451f8f50ecf596e5205273dc45ba3e008ad

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -23,15 +23,19 @@ make_EHelper(sub) {
   // TODO();
 
   // printf("src->val = 0x%08x\tsrc->width = %d\n", id_src->val, id_src->width);
-  if(id_src->width == 1 && id_dest->width != 1){
-	  // id_src->val = 0xf8;
-	  rtl_sext(&id_src->val, &id_src->val, id_src->width);
-	  id_src->width = id_dest->width;
-  }
+  rtl_sext(&id_src->val, &id_src->val, id_src->width);
+  rtl_sext(&id_dest->val, &id_dest->val, id_dest->width);
+  
   rtl_sub(&t0, &id_dest->val, &id_src->val);
-  if((id_src->val < 0 && id_dest->val > 0 && t0 < 0) || (id_src->val < 0 && id_dest->val > 0 && t0 > 0)) cpu.OF = 1;
-  else cpu.OF = 0;
-  cpu.CF = t0 > id_dest->val?1:0;
+  rtl_sltu(&t1, &id_dest->val, &id_src->val);
+  rtl_set_CF(&t1);
+
+  rtl_xor(&t1, &id_dest->val, &id_src->val);
+  rtl_xor(&t2, &t0, &id_dest->val);
+  rtl_and(&t1, &t1, &t2);
+  rtl_msb(&t1, &t1, id_dest->width);
+  rtl_set_OF(&t1);
+  
   operand_write(id_dest, &t0);
   rtl_update_ZFSF(&t0, id_dest->width);
   // printf("src->val = 0x%08x\tsrc->width = %d\n", id_src->val, id_src->width);
@@ -57,6 +61,7 @@ make_EHelper(cmp) {
 	rtl_and(&t1, &t1, &t2);
 	rtl_msb(&t1, &t1, id_dest->width);
 	rtl_set_OF(&t1);
+	
 	rtl_update_ZFSF(&t0, id_dest->width);
 	// printf("cpu.ZF = %d\n", cpu.ZF);
 	print_asm_template2(cmp);
