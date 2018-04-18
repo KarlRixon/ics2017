829a465a82a60074bf4611ca3d4f2350831f26fb
tracer-ics2017 <tracer@njuics.org>
2018-04-18 20:43:27 +0800

    >  compile 161620222 karl Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  20:43:27 up 10:34, 18 users,  load average: 0.08, 0.02, 0.01 5c275959a7544341b853a62c13595b662e24d0f0

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -253,6 +253,7 @@ make_DHelper(gp2_cl2E) {
 make_DHelper(gp2_Ib2E) {
   printf(c_bold c_red "hi\n" c_normal);
   decode_op_rm(eip, id_dest, true, NULL, false);
+  printf(c_bold c_red "hi\n" c_normal);
   id_src->width = 1;
   decode_op_I(eip, id_src, true);
 }
