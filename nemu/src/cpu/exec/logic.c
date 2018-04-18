6c4ee4a85d09a2576edd4876d14ed7bec2d72195
tracer-ics2017 <tracer@njuics.org>
2018-04-18 21:39:52 +0800

    >  compile 161620222 karl Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  21:39:51 up 11:30, 18 users,  load average: 0.00, 0.00, 0.00 5b88bd5d2bf90857b35bc962ae09742e585bed90

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -68,15 +68,7 @@ make_EHelper(sar) {
   // unnecessary to update CF and OF in NEMU
   // printf("src1->width = %d\tdest->width = %ddest->type = %d\n", id_src->width, id_dest->width, id_dest->type);
   // printf("src1 = 0x%08x\tsrc2 = 0x%08x\tdest->reg = 0x%08x\n", id_src->val, id_src2->val, id_dest->reg);
-  t0 = id_src->val;
-  while(t0){
-	  rtl_lr(&t1, id_dest->val, id_dest->width);
-	  // printf("id_dest->regval = 0x%08x\n", t1);
-	  rtl_andi(&t2, &t1, 0x00000001);
-	  // rtl_set_CF(&t2);
-	  rtl_sari(&t1, &t1, 0x1);
-	  t0 = t0 - 1;
-  }
+  rtl_sar(&t0, &id_dest->val, &id_src->val);
   operand_write(id_dest, &t0);
   // if(t0 == 1) rtl_set_OF(&tzero);
   print_asm_template2(sar);
