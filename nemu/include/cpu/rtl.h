32ac22528492faf258405cef4e5911817c2e853e
tracer-ics2017 <tracer@njuics.org>
2018-04-18 13:23:06 +0800

    >  compile 161620222 karl Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  13:23:06 up  3:14, 18 users,  load average: 0.05, 0.02, 0.00 23b64db02998f1489ca075751aada0f4ec273bee

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -147,8 +147,8 @@ static inline void rtl_push(const rtlreg_t* src1, int width) {
   // esp <- esp - 4
   // M[esp] <- src1
   // TODO();
-  cpu.esp -= width;
-  rtl_sm(&cpu.esp, 4, src1);
+  cpu.esp -= 4;
+  rtl_sm(&cpu.esp, width, src1);
 }
 
 static inline void rtl_pop(rtlreg_t* dest) {
