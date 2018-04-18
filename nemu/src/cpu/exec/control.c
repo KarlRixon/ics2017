ac8b9dc19d369eaba846cf97e6dd63957e0c3ae7
tracer-ics2017 <tracer@njuics.org>
2018-04-18 17:55:22 +0800

    >  compile 161620222 karl Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  17:55:22 up  7:46, 18 users,  load average: 0.00, 0.00, 0.00 5bcf5aa256a71a9d113632227a8ec5ee00b8b09

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -16,7 +16,7 @@ make_EHelper(jcc) {
   rtl_sext(&id_dest->val, &id_dest->val, id_dest->width);
   decoding.jmp_eip = decoding.seq_eip + id_dest->val;
   // printf("dest->width = %x\n", id_dest->width);
-  printf("seq_eip = 0x%08x\tdest_.val = 0x%08x\tjmp_eip = 0x%08x\n", decoding.seq_eip, id_dest->val, decoding.jmp_eip);
+  // printf("seq_eip = 0x%08x\tdest_.val = 0x%08x\tjmp_eip = 0x%08x\n", decoding.seq_eip, id_dest->val, decoding.jmp_eip);
   decoding.is_jmp = t2;
 
   print_asm("j%s %x", get_cc_name(subcode), decoding.jmp_eip);
