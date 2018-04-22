c499bbc7b26d699be5c377b8a0b1b443135faaea
tracer-ics2017 <tracer@njuics.org>
2018-04-22 16:54:59 +0800

    >  compile 161620222 karl Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  16:54:59 up  3:22, 14 users,  load average: 0.07, 0.03, 0.00 c51f3a26928b9cc1e6c4ee16d59c65cf8db8ba4

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -50,6 +50,7 @@ make_EHelper(in) {
   t0 = pio_read(id_src->val, id_dest->width);
   printf("t0 = 0x%08x\n", t0);
   rtl_mv(&id_dest->val, &t0);
+  printf("dest = 0x%08x\n", id_dest->val);
   print_asm_template2(in);
 
 #ifdef DIFF_TEST
