3a8c27bf8ed3a168af83cbc90c4a11bda393d630
tracer-ics2017 <tracer@njuics.org>
2018-04-28 23:48:47 +0800

    >  compile 161620222 karl Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  23:48:47 up  1:41, 12 users,  load average: 0.06, 0.35, 0.23 212617a45a9057e34b4228eca68c7be1d0c6312

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -115,6 +115,7 @@ make_EHelper(neg) {
 }
 
 make_EHelper(adc) {
+  rtl_sext(&id_src->val, &id_src->val, id_src->width);
   rtl_add(&t2, &id_dest->val, &id_src->val);
   rtl_sltu(&t3, &t2, &id_dest->val);
   rtl_get_CF(&t1);
