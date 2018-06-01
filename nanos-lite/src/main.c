f5cfb27b8a682c22373c4aac225c8e9cff413f2f
tracer-ics2017 <tracer@njuics.org>
2018-06-02 01:59:54 +0800

    >  run 161620222 karl Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  01:59:54 up  2:44,  5 users,  load average: 0.00, 0.00, 0.02 ea27dd109cd61f597dc79dd82d75dc6997a3730

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -30,7 +30,9 @@ int main() {
 
   init_fs();
 
-  uint32_t entry = loader(NULL, NULL);
+  //uint32_t entry = loader(NULL, NULL);
+  uint32_t entry = loader(NULL, "/bin/text");
+  Log("entry : %d", entry);
   ((void (*)(void))entry)();
 
   panic("Should not reach here");
