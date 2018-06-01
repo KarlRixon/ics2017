dbf277a70d6b347a0138571b51b56dc4715f0e3b
tracer-ics2017 <tracer@njuics.org>
2018-06-02 02:29:15 +0800

    >  run 161620222 karl Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  02:29:15 up  3:13,  7 users,  load average: 0.50, 1.22, 0.85 e2322346a25ca154752243103fd5ebe12daefd5c

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,11 +1,12 @@
 #include "common.h"
 #include "syscall.h"
 
-//extern ssize_t fs_read(int fd, void *buf, size_t len);
-//extern ssize_t fs_write(int fd, const void *buf, size_t len);
-//extern int fs_open(const char *pathname, int flags, int mode);
-//extern off_t fs_lseek(int fd, off_t offset, int whence);
-//extern int fs_close(int fd);
+int fs_open(const char *pathname, int flags, int mode);
+ssize_t fs_read(int fd, void *buf, size_t len);
+ssize_t fs_write(int fd, const void *buf, size_t len);
+off_t fs_lseek(int fd, off_t offset, int whence);
+int fs_close(int fd);
+int mm_brk(uint32_t new_brk);
 
 static inline _RegSet* sys_none(_RegSet *r) {
 	SYSCALL_ARG1(r) = 1;
@@ -18,19 +19,61 @@ static inline _RegSet* sys_exit(_RegSet *r) {
 }
 
 static inline _RegSet* sys_write(_RegSet *r) {
+	//Log("!");
+	//if(SYSCALL_ARG2(r) == 1 || SYSCALL_ARG2(r) == 2){
+	//	for(int i = 0; i < SYSCALL_ARG4(r); i++) {
+	//		_putc(((char*)SYSCALL_ARG3(r))[i]);
+	//	}
+	//	SYSCALL_ARG1(r) = SYSCALL_ARG4(r);
+	//}
+	//return NULL;
+	
 	Log("!");
-	if(SYSCALL_ARG2(r) == 1 || SYSCALL_ARG2(r) == 2){
-		for(int i = 0; i < SYSCALL_ARG4(r); i++) {
-			_putc(((char*)SYSCALL_ARG3(r))[i]);
-		}
-		SYSCALL_ARG1(r) = SYSCALL_ARG4(r);
-	}
+	int fd = (int)SYSCALL_ARG2(r);
+	const void *buf = (const void *)SYSCALL_ARG3(r);
+	size_t len = (size_t)SYSCALL_ARG4(r);
+	SYSCALL_ARG1(r) = fs_write(fd, buf, len);
 	return NULL;
 }
 
 static inline _RegSet* sys_brk(_RegSet *r) {
-	SYSCALL_ARG1(r) = 0;
+	//SYSCALL_ARG1(r) = 0;
+	//return NULL;
+		  
+	// printf("%x\n", _heap.end);
+	uint32_t new_brk = SYSCALL_ARG2(r);
+	SYSCALL_ARG1(r) = mm_brk(new_brk);
 	return NULL;
+}
+
+static inline _RegSet* sys_open(_RegSet *r) {
+  const char *pathname = (const char *)SYSCALL_ARG2(r);
+  int flags = (int)SYSCALL_ARG3(r);
+  int mode = (int)SYSCALL_ARG4(r);
+  SYSCALL_ARG1(r) = fs_open(pathname, flags, mode);
+  return NULL;
+}
+
+static inline _RegSet* sys_read(_RegSet *r) {
+  int fd = (int)SYSCALL_ARG2(r);
+  void *buf = (void *)SYSCALL_ARG3(r);
+  size_t len = (size_t)SYSCALL_ARG4(r);
+  SYSCALL_ARG1(r) = fs_read(fd, buf, len);
+  return NULL;
+}
+
+static inline _RegSet* sys_lseek(_RegSet *r) {
+  int fd = (int)SYSCALL_ARG2(r);
+  off_t offset = (off_t)SYSCALL_ARG3(r);
+  int whence = (int)SYSCALL_ARG4(r);
+  SYSCALL_ARG1(r) = fs_lseek(fd, offset, whence);
+  return NULL;
+}
+
+static inline _RegSet* sys_close(_RegSet *r) {
+  int fd = (int)SYSCALL_ARG2(r);
+  SYSCALL_ARG1(r) = fs_close(fd);
+  return NULL;
 }
 
 _RegSet* do_syscall(_RegSet *r) {
@@ -49,6 +92,14 @@ _RegSet* do_syscall(_RegSet *r) {
 		return sys_write(r);
 	case SYS_brk:
 		return sys_brk(r);
+	case SYS_open:
+		return sys_open(r);
+	case SYS_read:
+		return sys_read(r);
+	case SYS_close:
+		return sys_close(r);
+	case SYS_lseek:
+		return sys_lseek(r);
 
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
