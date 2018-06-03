a54ed55109053c39c446b872cf8601286bb28770
tracer-ics2017 <tracer@njuics.org>
2018-06-03 23:56:46 +0800

    >  run 161620222 karl Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  23:56:45 up 2 days, 39 min, 11 users,  load average: 0.76, 0.45, 0.18 43555b3ab9b7941dcff2705e195d8c9e62b035d

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -2,8 +2,8 @@
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
-//extern void ramdisk_read(void *buf, off_t offset, size_t len);
-//extern size_t get_ramdisk_size();
+extern void ramdisk_read(void *buf, off_t offset, size_t len);
+extern size_t get_ramdisk_size();
 
 void* new_page(void);
 
@@ -14,8 +14,8 @@ int fs_close(int fd);
 
 uintptr_t loader(_Protect *as, const char *filename) {
   // TODO();
-  //ramdisk_read(DEFAULT_ENTRY, 0, get_ramdisk_size());
-  //return (uintptr_t)DEFAULT_ENTRY;
+  ramdisk_read(DEFAULT_ENTRY, 0, get_ramdisk_size());
+  return (uintptr_t)DEFAULT_ENTRY;
 
   int fd = fs_open(filename, 0, 0);
   int i, pages = fs_filesz(fd) / PGSIZE + 1;
