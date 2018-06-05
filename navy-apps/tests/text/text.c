40241dd7fb986e76d49280735286537e95c4ec55
tracer-ics2017 <tracer@njuics.org>
2018-06-06 00:21:40 +0800

    >  run 161620222 karl Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  00:21:40 up 4 days,  1:03, 12 users,  load average: 0.00, 0.00, 0.10 82c0afbbe826a7ad7e312344eac526ec989b6849

--- navy-apps/tests/text/text.c
+++ navy-apps/tests/text/text.c
@@ -20,17 +20,18 @@ int main() {
     assert(n == i + 1);
   }
 
-  printf("PASS!!!\n");
+  //printf("PASS!!!\n");
 
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fprintf(fp, "%4d\n", i + 1 + 1000);
   }
 
-  printf("PASS!!!\n");
+  //printf("PASS!!!\n");
 
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
+    printf("n = %d\n", n);
     assert(n == i + 1);
   }
 
