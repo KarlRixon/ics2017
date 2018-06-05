ac7c000e1a12747314f2f569e52845bac3a1d898
tracer-ics2017 <tracer@njuics.org>
2018-06-05 23:17:38 +0800

    >  run 161620222 karl Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  23:17:38 up 3 days, 23:59, 11 users,  load average: 0.24, 0.07, 0.02 1e13952082257f8935278d10e82611ae60b0431f

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -18,16 +18,19 @@ uintptr_t loader(_Protect *as, const char *filename) {
   //return (uintptr_t)DEFAULT_ENTRY;
 
   int fd = fs_open(filename, 0, 0);
-  int i, pages = fs_filesz(fd) / PGSIZE + 1;
-  void *pa, *va;
+  fs_read(fd, 0, fs_filesz(fd));
+
+  //int fd = fs_open(filename, 0, 0);
+  //int i, pages = fs_filesz(fd) / PGSIZE + 1;
+  //void *pa, *va;
 		  
-  for (i = 0, va = DEFAULT_ENTRY; i < pages; ++i, va += PGSIZE) {
-	  pa = new_page();
-	  printf("pa: %x, va: %x", pa, va);
-	  printf(", read: %d\n", fs_read(fd, pa, PGSIZE));
-	  fs_read(fd, pa, PGSIZE);
-      _map(as, va, pa);
-  }
+  //for (i = 0, va = DEFAULT_ENTRY; i < pages; ++i, va += PGSIZE) {
+//	  pa = new_page();
+//	  printf("pa: %x, va: %x", pa, va);
+//	  printf(", read: %d\n", fs_read(fd, pa, PGSIZE));
+//	  fs_read(fd, pa, PGSIZE);
+//	  _map(as, va, pa);
+  //}
 
   fs_close(fd);
   return (uintptr_t)DEFAULT_ENTRY;
