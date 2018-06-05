7e1aa8d7b99fb3e6097a5542fa77e6c3f7124e19
tracer-ics2017 <tracer@njuics.org>
2018-06-05 23:35:31 +0800

    >  run 161620222 karl Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  23:35:31 up 4 days, 17 min, 11 users,  load average: 0.19, 0.30, 0.25 89a267ff91fbf040a1588c17e970a29ceeaded5

--- navy-apps/tests/text/text.c
+++ navy-apps/tests/text/text.c
@@ -9,6 +9,8 @@ int main() {
   long size = ftell(fp);
   assert(size == 5000);
 
+  printf("PASS!!!\n");
+  
   fseek(fp, 500 * 5, SEEK_SET);
   int i, n;
   for (i = 500; i < 1000; i ++) {
@@ -16,15 +18,21 @@ int main() {
     assert(n == i + 1);
   }
 
+  printf("PASS!!!\n");
+
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fprintf(fp, "%4d\n", i + 1 + 1000);
   }
 
+  printf("PASS!!!\n");
+
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
     assert(n == i + 1);
   }
+
+  printf("PASS!!!\n");
 
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
