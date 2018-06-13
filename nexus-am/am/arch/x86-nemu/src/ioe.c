549b41282524446650bccc685bcfafdd424a71a0
tracer-ics2017 <tracer@njuics.org>
2018-06-13 16:07:06 +0800

    >  run 161620222 karl Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  16:07:06 up  1:49,  9 users,  load average: 1.18, 0.61, 0.30 709bcd1ce99ffa0abd55843dce72dc4c72a03c26

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -18,8 +18,8 @@ unsigned long _uptime() {
 uint32_t* const fb = (uint32_t *)0x40000;
 
 _Screen _screen = {
-  .width  = 400,
-  .height = 300,
+  .width  = 480,
+  .height = 360,
 };
 
 extern void* memcpy(void *, const void *, int);
