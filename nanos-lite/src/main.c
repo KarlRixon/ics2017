653f07bae7b11320fcdf30c15e5073a6678392a5
tracer-ics2017 <tracer@njuics.org>
2018-06-14 13:25:08 +0800

    >  run 161620222 karl Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  13:25:08 up 36 min,  8 users,  load average: 0.20, 0.05, 0.02 79740f8b5380adb6ada2944231c178a03e288438

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -31,11 +31,11 @@ int main() {
   init_fs();
 
   //uint32_t entry = loader(NULL, NULL);
-  uint32_t entry = loader(NULL, "/bin/dummy");
+  //uint32_t entry = loader(NULL, "/bin/dummy");
   //uint32_t entry = loader(NULL, "/bin/hello");
   //uint32_t entry = loader(NULL, "/bin/text");
   //uint32_t entry = loader(NULL, "/bin/bmptest");
-  //uint32_t entry = loader(NULL, "/bin/pal");
+  uint32_t entry = loader(NULL, "/bin/pal");
   //uint32_t entry = loader(NULL, "/bin/events");
 
   Log("entry : %d", entry);
