e91047f604ee8ef260e3f6e31a53e45116b465e5
tracer-ics2017 <tracer@njuics.org>
2018-06-18 21:58:40 +0800

    >  compile 161620222 karl Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  21:58:40 up  7:16,  5 users,  load average: 0.02, 0.01, 0.00 a0a01843d8304edf3e389f4b80444c866e35c39

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -2,6 +2,7 @@
 #define __REG_H__
 
 #include "common.h"
+#include "memory/mmu.h"
 
 enum { R_EAX, R_ECX, R_EDX, R_EBX, R_ESP, R_EBP, R_ESI, R_EDI };
 enum { R_AX, R_CX, R_DX, R_BX, R_SP, R_BP, R_SI, R_DI };
@@ -55,14 +56,8 @@ typedef struct {
 		uint32_t base;
 	}idtr;
 	
-	union{
-		struct{
-			uint32_t :31;
-			uint32_t PG:1;
-		};
-		uint32_t cr0;
-	};
-	uint32_t cr3;
+	CR0 cr0;
+	CR3 cr3;
 
 } CPU_state;
 
