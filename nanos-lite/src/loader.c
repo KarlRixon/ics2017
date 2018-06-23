b42cd0e2771072b89e15c189ac39dc7a4cb939f4
tracer-ics2017 <tracer@njuics.org>
2018-06-24 02:54:00 +0800

    >  run 161620222 karl Linux debian 4.9.0-4-686-pae #1 SMP Debian 4.9.65-3+deb9u1 (2017-12-23) i686 GNU/Linux  02:54:00 up 10:01, 15 users,  load average: 0.00, 0.00, 0.00 3047b2f46919259a9bb1018b32ee6a257aa47a20

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -26,11 +26,11 @@ uintptr_t loader(_Protect *as, const char *filename) {
   void *va;
 
   Log("loaded: [%d]%s size:%d", fd, filename, nbyte);
-  //Log("Map va to pa: 0x%08x to 0x%08x", va, pa);
 
   void *end = DEFAULT_ENTRY + nbyte;
   for (va = DEFAULT_ENTRY; va < end; va += PGSIZE) {
 	  pa = new_page();
+	  Log("Map va to pa: 0x%08x to 0x%08x", va, pa);
 	  _map(as, va, pa);
   	  fs_read(fd, pa, (end - va) < PGSIZE ? (end - va) : PGSIZE);
   }
